f8eab835bb729260a5187524de51d28f2b67a283
Aneek <aneekmallick20@gmail.com>
2025-03-04 19:37:44 +0530

    updatev1.5

--- models/text_processing.py
+++ models/text_processing.py
@@ -1,14 +1,6 @@
-import spacy
 import re
-
-nlp = spacy.load("../en_core_web_sm")
 
 def preprocess_text(text):
     text = text.lower()
-    
-    text = re.sub(r"[^\w\s]", "", text)
-    doc = nlp(text)
-
-    processed_text = " ".join([token.lemma_ for token in doc if not token.is_stop])
-    
-    return processed_text
+    text = re.sub(r"\s+", " ", text)
+    return text
