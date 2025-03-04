1a2a97875d7b97036e77783cd824eeaf8c2a221b
Aneek <aneekmallick20@gmail.com>
2025-03-04 18:58:08 +0530

    updatev1.1

--- models/text_processing.py
+++ models/text_processing.py
@@ -1,7 +1,12 @@
-import spacy
 import re
+import spacy
+import subprocess
 
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp=spacy.load("en_core_web_sm")
+except OSError:
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+    nlp=spacy.load("en_core_web_sm")
 
 def preprocess_text(text):
     text = text.lower()
